ab046f5e4b8daafafcea34c41d81049c8a2e83f4
8Mixaji888 <gomixajil@yandex.ru>
2026-03-28 20:24:45 +0300

    AC

--- maze.py
+++ maze.py
@@ -1,4 +1,5 @@
 #создай игру "Лабиринт"!
+from re import S
 from pygame import *
 
 class GameSprite(sprite.Sprite):
@@ -36,13 +37,28 @@ class Player(GameSprite):
         if keys[K_DOWN] and self.rect.y < mwHeight-65:
             self.rect.y += self.speed
 
+class Enemy(GameSprite):
+    def __init__(self, ppicture, xcor, ycor, speed=0):
+        super().__init__(ppicture, xcor, ycor, speed)
+        self.direction = 'left'
+
+    def go(self):       
+        if self.rect.x <= 470:
+            self.direction = 'right'
+        if self.rect.x >= 635:
+            self.direction = 'left'
+        if self.direction == 'left':
+            self.rect.x -= self.speed
+        else:
+            self.rect.x += self.speed
+
 game_over = False
 clock = time.Clock()
 mixer.music.load('jungles.ogg')
 mixer.music.play()
 
 player = Player('hero.png', 10, 420, 5)
-cyborg = GameSprite('cyborg.png', 625, 280)
+cyborg = Enemy('cyborg.png', 625, 280, 5)
 gold = GameSprite('treasure.png', 625, 425)
 
 while not game_over:
@@ -51,6 +67,7 @@ while not game_over:
             game_over = True
     
     player.go()
+    cyborg.go()
 
     mw.blit(background, (0,0))
     player.reset()
